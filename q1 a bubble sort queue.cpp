17f346625911435d38c6323f7c1c379346af6c21
Sudipta Biswas <31337864+SudiptaBiswas1804@users.noreply.github.com>
2018-02-10 09:17:24 +0530

    Update q1 a bubble sort queue.cpp

--- q1 a bubble sort queue.cpp
+++ q1 a bubble sort queue.cpp
@@ -1,33 +1,35 @@
+//Pragramme for bubble sorting of an array
 #include <iostream>
 using namespace std;
 
-void BubbleSort(int arr[],int n){//taking array and size of array as input
-	int i,j,k,temp = 0;
-	for(i=0;i<n;i++){//Controls number of cycles of sort
-		for(j=0;j<n-1;j++){//Controls number of swappings
-			if(arr[j]>arr[j+1]){
-				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
-				}
-		}
+//Main function for the programme
+int main() {
+int i,n,in,j,temp=0,k;
+int arr[n];
+cout<<"enter the size of the array "<<endl;
+cin>>n;
+cout<<"enter the array elements"<<endl;
+for(i=0; i<n; i++) {                 //Taking the values of the array elements
+	cin>>in;
+	arr[i]=in;
 	}
-	for(k=0;k<n;k++){//Displaying sorted array
-		cout<<arr[k]<<",";
-	}
-	cout<<endl;
+cout<<"the array elements are "<<endl;
+for(i=0; i<n; i++){                 //Displaying the original array
+cout<<arr[i]<<" ";
 }
-int main(){
-	int n,i,in,k = 0;
-	int arr[] = {0};
-	cout<<"Enter the number of elements in an array\n";
-	cin>>n;
-	for(i=0;i<n;i++){
-		cout<<"Enter value\n";
-		cin>>in;
-		arr[i]=in;
+for(i=0; i<n; i++){                 //Sorting (bubble sorting) the array
+	for(j=0; j<n-1; j++){
+		if(arr[j]>arr[j+1]){
+			temp=arr[j];
+			arr[j]=arr[j+1];
+			arr[j+1]=temp;
+		}
+		
 	}
-	cout<<"This is the sorted array\n";
-	BubbleSort(arr,n);//calling function to sort array
-	
+	}
+cout<<"\n"<<"the sorted array elements are "<<endl;             //Displaying the sorted array
+for(k=0; k<n; k++){          //Controls the number of cycles of sort     
+cout<<arr[k]<<" ";           //Controls the number of swapping
+}
+return 0;
 }

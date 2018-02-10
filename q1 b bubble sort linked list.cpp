f8bd446c3c42c0f61dc519101f6542a2b52b17b9
Sudipta Biswas <31337864+SudiptaBiswas1804@users.noreply.github.com>
2018-02-10 10:24:42 +0530

    Update q1 b bubble sort linked list.cpp

--- q1 b bubble sort linked list.cpp
+++ q1 b bubble sort linked list.cpp
@@ -1,78 +1,92 @@
+//Programme for bubble sort in linkedlist
 #include <iostream>
 using namespace std;
-struct node{//creating data structure of node type
+
+//Contruction of the node
+struct node{
 	int data;
-	node*next;
+	node *next;
 };
-class linkedlist{//creating class for list
-	node*head;
-	node*tail;
+
+//The linkedlist class
+class linkedlist{
+	private:
+	node *head;
+	node *tail;
 	public:
 		linkedlist(){
-			head = NULL;
-			tail = NULL;
+			head=NULL;
+			tail=NULL;
 		}
-		void insert(int value){//inserting elements
-			node*temp = new node;
-			temp->data = value;
-			if(head == NULL){
-				head = temp;
-				tail = temp;
-				temp->next = NULL;
+		
+		//Addition function - function for adding an element at one end 
+		void addnode(int n){
+			node *temp=new node;
+			temp->data=n;
+			if(head==NULL){
+				head=temp;
+				tail=temp;
 			}
 			else{
-				tail->next = temp;
-				tail = temp;
-				temp->next = NULL;
+				tail->next=temp;
+				tail=temp;
 			}
 		}
-			void display(){
-			node*temp = new node;
-			temp = head;
+		//Displaying function - function for displaying the elements of the linked list
+		void displaynode(){
+			node *temp=new node;
+			temp=head;
 			while(temp!=NULL){
-				cout<<temp->data;
-				temp = temp->next;
+				cout<<temp->data<<"->";
+				temp=temp->next;
 			}
+			cout<<"NULL"<<endl;
 		}
-		void BubbleSort(){
-			node*curr = new node;
-			node*prev = new node;
-			node*timekeep = new node;//to keep track of number of cycles of sort
-			int temp = 0;
-			curr = head;
-			timekeep = head;
-			while(timekeep!=NULL){
-			
-			while(curr!=NULL){
-				prev = curr;
-				curr = curr->next;
-				if(prev->data>curr->data){//comparing values
-					temp = prev->data;
-					prev->data = curr->data;//swapping values
-					curr->data = temp;
+		
+		//To count the number of elements in the list
+		int countnode() 
+		{       int count=0;
+		        node *temp=new node;
+	                    temp= head;
+	                    while(temp!=NULL)
+                                    { temp=temp->next;
+                                      count++;
+                                    }
+	                    return count;
+                        }	
+	
+		//Sorting function - function for bubble sorting of the linked list
+		int sortnode(int n){
+			node *cur=new node;
+			node *prev=new node;
+			for(int i=0; i<n; i++){
+				prev=head;
+				for(int j=0; j<(n-i)-1; j++){
+					if(prev->data>prev->next->data){
+						cur->data=prev->data;
+						prev->data=prev->next->data;
+						prev->next->data=cur->data;
+					}
+				prev=prev->next;
 				}
 			}
-			timekeep = timekeep->next;
+			return 0;
+			
 		}
-		display();//to display sorted linked list
-		}
-	
+		
 };
+
 int main(){
-	int val,in,i,j = 0;
-	linkedlist ob;
-	while(j!=9){
-		cout<<"Enter 1 to add element to linked list or press 2 to exit\n";
-		cin>>i;
-		if(i==1){
-			cout<<"Enter value\n";
-			cin>>val;
-			ob.insert(val);
-		}
-		if(i==2){
-			j=9;
-		}
-	}
-	cout<<"This is the sorted list\n";
-	ob.BubbleSort();
+	int n;
+	linkedlist l;
+	l.addnode(2);
+	l.addnode(1);
+	l.addnode(6);
+	cout<<"The elements of linked list without sorting is : "<<endl;
+	l.displaynode();
+	n=l.countnode();
+            l.sortnode(n);
+	cout<<"The elements of linked list with sorting is : "<<endl;
+            l.displaynode();
+            return 0;
 }
